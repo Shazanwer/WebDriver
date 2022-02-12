3cb64eb99f83bf481f99b9fe2b22ca715eeefcf2
shazanwer <shazanwer@gmail.com>
2022-02-12 12:07:10 +0300

    updated

--- src/test/LoginTest.java
+++ src/test/LoginTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -15,10 +16,13 @@ public class LoginTest {
 		// TODO Auto-generated method stub
 
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-
 		System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
 //		WebDriver driver = new FirefoxDriver();
+
+		
+		
 
 		driver.get("https://www.simplilearn.com");
 
